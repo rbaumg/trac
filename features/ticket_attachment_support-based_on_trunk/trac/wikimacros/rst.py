be7379dd9cfc37b116b80556fb09ea828083a528
rocky <rocky@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2004-04-11 19:38:31 +0000

    No longer need ticket_attachment_support-based_on_trunk branch.
    git-svn-id: https://svn.edgewall.org/repos/trac/branches/rocky-dev@392 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- features/ticket_attachment_support-based_on_trunk/trac/wikimacros/rst.py
+++
@@ -1,6 +0,0 @@
-from docutils.core import publish_string
-
-def execute(hdf, text):
-    html = publish_string(text, writer_name = 'html')
-    return html[html.find('<body>')+6:html.find('</body>')].strip()
-
