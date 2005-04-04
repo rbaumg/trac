fe2e93909fd89de3f007b5eff1cc0c74fe294816
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2005-04-04 08:16:42 +0000

    ''category-branch'': home for the #919 patch
    git-svn-id: https://svn.edgewall.org/repos/trac/branches/cboos-dev@1488 af82e41b-90c4-0310-8c96-b1721e28e2e2

---
+++ category-branch/trac/Changeset.py
@@ -0,0 +1,229 @@
+# -*- coding: iso8859-1 -*-
+#
+# Copyright (C) 2003, 2004, 2005 Edgewall Software
+# Copyright (C) 2003, 2004, 2005 Jonas Borgström <jonas@edgewall.com>
+#
+# Trac is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Trac is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# Author: Jonas Borgström <jonas@edgewall.com>
+
+from trac import perm
+from trac.Module import Module
+from trac.versioncontrol import Changeset, Node
+from trac.versioncontrol.diff import get_diff_options, hdf_diff, unified_diff
+from trac.web.main import add_link
+from trac.WikiFormatter import wiki_to_html
+
+import time
+import util
+import re
+
+
+class ChangesetModule(Module):
+
+    def render(self, req):
+        self.perm.assert_permission(perm.CHANGESET_VIEW)
+
+        add_link(req, 'alternate', '?format=diff', 'Unified Diff',
+                 'text/plain', 'diff')
+        add_link(req, 'alternate', '?format=zip', 'Zip Archive',
+                 'application/zip', 'zip')
+
+        rev = req.args.get('rev')
+        repos = self.env.get_repository(req.authname)
+
+        diff_options = get_diff_options(req)
+        if req.args.has_key('update'):
+            req.redirect(self.env.href.changeset(rev))
+
+        chgset = repos.get_changeset(rev)
+        req.check_modified(chgset.date, diff_options[0] + ''.join(diff_options[1]))
+
+        format = req.args.get('format')
+        if format == 'diff':
+            self.render_diff(req, repos, chgset, diff_options)
+        elif format == 'zip':
+            self.render_zip(req, repos, chgset)
+        else:
+            self.render_html(req, repos, chgset, diff_options)
+
+    def render_html(self, req, repos, chgset, diff_options):
+        """HTML version"""
+
+        req.hdf['title'] = '[%s]' % chgset.rev
+        req.hdf['changeset'] = {
+            'revision': chgset.rev,
+            'time': time.asctime(time.localtime(chgset.date)),
+            'author': util.escape(chgset.author or 'anonymous'),
+            'message': wiki_to_html(util.wiki_escape_newline(chgset.message or '--'),
+                                    req.hdf, self.env, self.db)
+        }
+
+        oldest_rev = repos.oldest_rev
+        if chgset.rev != oldest_rev:
+            add_link(req, 'first', self.env.href.changeset(oldest_rev),
+                     'Changeset %s' % oldest_rev)
+            previous_rev = repos.previous_rev(chgset.rev)
+            add_link(req, 'prev', self.env.href.changeset(previous_rev),
+                     'Changeset %s' % previous_rev)
+        youngest_rev = repos.youngest_rev
+        if chgset.rev != youngest_rev:
+            next_rev = repos.next_rev(chgset.rev)
+            add_link(req, 'next', self.env.href.changeset(next_rev),
+                     'Changeset %s' % next_rev)
+            add_link(req, 'last', self.env.href.changeset(youngest_rev),
+                     'Changeset %s' % youngest_rev)
+
+        edits = []
+        idx = 0
+        for path, kind, change, base_path, base_rev in chgset.get_changes():
+            info = {'change': change}
+            if base_path:
+                info['path.old'] = base_path
+                info['rev.old'] = base_rev
+                info['browser_href.old'] = self.env.href.browser(base_path,
+                                                                 rev=base_rev)
+            if path:
+                info['path.new'] = path
+                info['rev.new'] = chgset.rev
+                info['browser_href.new'] = self.env.href.browser(path,
+                                                                 rev=chgset.rev)
+            if change in (Changeset.COPY, Changeset.EDIT, Changeset.MOVE):
+                edits.append((idx, path, kind, base_path, base_rev))
+            req.hdf['changeset.changes.%d' % idx] = info
+            idx += 1
+
+        for idx, path, kind, base_path, base_rev in edits:
+            old_node = repos.get_node(base_path or path, base_rev)
+            new_node = repos.get_node(path, chgset.rev)
+
+            # Property changes
+            old_props = old_node.get_properties()
+            new_props = new_node.get_properties()
+            changed_props = {}
+            if old_props != new_props:
+                for k,v in old_props.items():
+                    if not k in new_props:
+                        changed_props[k] = {'old': v}
+                    elif v != new_props[k]:
+                        changed_props[k] = {'old': v, 'new': new_props[k]}
+                for k,v in new_props.items():
+                    if not k in old_props:
+                        changed_props[k] = {'new': v}
+                req.hdf['changeset.changes.%d.props' % idx] = changed_props
+
+            if kind == Node.DIRECTORY:
+                continue
+
+            # Content changes
+            old_content = old_node.get_content().read()
+            if self.env.mimeview.is_binary(old_content):
+                continue
+            new_content = new_node.get_content().read()
+            if old_content != new_content:
+                context = 3
+                for option in diff_options:
+                    if option[:2] == '-U':
+                        context = int(option[2:])
+                        break
+                tabwidth = int(self.env.get_config('diff', 'tab_width', '8'))
+                changes = hdf_diff(old_content.splitlines(),
+                                   new_content.splitlines(),
+                                   context, tabwidth,
+                                   ignore_blank_lines='-B' in diff_options,
+                                   ignore_case='-i' in diff_options,
+                                   ignore_space_changes='-b' in diff_options)
+                req.hdf['changeset.changes.%d.diff' % idx] = changes
+
+        req.display('changeset.cs')
+
+    def render_diff(self, req, repos, chgset, diff_options):
+        """Raw Unified Diff version"""
+        req.send_response(200)
+        req.send_header('Content-Type', 'text/plain;charset=utf-8')
+        req.send_header('Content-Disposition',
+                        'filename=Changeset%s.diff' % req.args.get('rev'))
+        req.end_headers()
+
+        for path, kind, change, base_path, base_rev in chgset.get_changes():
+            if change is Changeset.ADD:
+                old_node = None
+            else:
+                old_node = repos.get_node(base_path or path, base_rev)
+            if change is Changeset.DELETE:
+                new_node = None
+            else:
+                new_node = repos.get_node(path, chgset.rev)
+
+            # TODO: Property changes
+
+            # Content changes
+            if kind is 'dir':
+                continue
+            new_content = old_content = ''
+            new_node_info = old_node_info = ('','')
+            if old_node:
+                old_content = old_node.get_content().read()
+                old_node_info = (old_node.path, old_node.rev)
+            if self.env.mimeview.is_binary(old_content):
+                continue
+            if new_node:
+                new_content = new_node.get_content().read()
+                new_node_info = (new_node.path, new_node.rev)                
+            if old_content != new_content:
+                context = 3
+                for option in diff_options:
+                    if option[:2] == '-U':
+                        context = int(option[2:])
+                        break
+                req.write('Index: ' + path + util.CRLF)
+                req.write('=' * 67 + util.CRLF)
+                req.write('--- %s (revision %s)' % old_node_info +
+                          util.CRLF)
+                req.write('+++ %s (revision %s)' % new_node_info +
+                          util.CRLF)
+                for line in unified_diff(old_content.splitlines(),
+                                         new_content.splitlines(), context,
+                                         ignore_blank_lines='-B' in diff_options,
+                                         ignore_case='-i' in diff_options,
+                                         ignore_space_changes='-b' in diff_options):
+                    req.write(line + util.CRLF)
+
+    def render_zip(self, req, repos, chgset):
+        """ZIP archive with all the added and/or modified files."""
+        req.send_response(200)
+        req.send_header('Content-Type', 'application/zip')
+        req.send_header('Content-Disposition',
+                        'filename=Changeset%s.zip' % chgset.rev)
+        req.end_headers()
+
+        try:
+            from cStringIO import StringIO
+        except ImportError:
+            from StringIO import StringIO
+        from zipfile import ZipFile, ZipInfo, ZIP_DEFLATED
+
+        buf = StringIO()
+        zipfile = ZipFile(buf, 'w', ZIP_DEFLATED)
+        for path, kind, change, base_path, base_rev in chgset.get_changes():
+            if kind is Node.FILE and change is not Changeset.DELETE:
+                node = repos.get_node(path, chgset.rev)
+                zipinfo = ZipInfo()
+                zipinfo.filename = node.path
+                zipinfo.date_time = time.gmtime(node.last_modified)[:6]
+                zipinfo.compress_type = ZIP_DEFLATED
+                zipfile.writestr(zipinfo, node.get_content().read())
+        zipfile.close()
+        req.write(buf.getvalue())
---
+++ category-branch/trac/tests/ticket.py
@@ -0,0 +1,57 @@
+from trac.Ticket import Ticket
+
+import unittest
+
+
+class TicketTestCase(unittest.TestCase):
+
+    def setUp(self):
+        from trac.test import InMemoryDatabase
+        self.db = InMemoryDatabase()
+
+    def test_create_ticket(self):
+        """Testing Ticket.insert()"""
+        # Multiple test in one method, this sucks
+        # 1. Creating ticket
+        ticket = Ticket()
+        ticket['reporter'] = 'santa'
+        ticket['summary'] = 'Foo'
+        ticket['custom_foo'] = 'This is a custom field'
+        self.assertEqual('santa', ticket['reporter'])
+        self.assertEqual('Foo', ticket['summary'])
+        self.assertEqual('This is a custom field', ticket['custom_foo'])
+        ticket.insert(self.db)
+
+        # Retrieving ticket
+        ticket2 = Ticket(self.db, 1)
+        self.assertEqual(1, ticket2['id'])
+        self.assertEqual('santa', ticket2['reporter'])
+        self.assertEqual('Foo', ticket2['summary'])
+        self.assertEqual('This is a custom field', ticket2['custom_foo'])
+
+        # Modifying ticket
+        ticket2['summary'] = 'Bar'
+        ticket2['custom_foo'] = 'New value'
+        ticket2.save_changes(self.db, 'santa', 'this is my comment')
+
+        # Retrieving ticket
+        ticket3 = Ticket(self.db, 1)
+        self.assertEqual(1, ticket3['id'])
+        self.assertEqual(ticket3['reporter'], 'santa')
+        self.assertEqual(ticket3['summary'], 'Bar')
+        self.assertEqual(ticket3['custom_foo'], 'New value')
+
+        # Testing get_changelog()
+        log = ticket3.get_changelog(self.db)
+        self.assertEqual(len(log), 3)
+        ok_vals = ['foo', 'summary', 'comment']
+        self.failUnless(log[0][2] in ok_vals)
+        self.failUnless(log[1][2] in ok_vals)
+        self.failUnless(log[2][2] in ok_vals)
+
+def suite():
+    return unittest.makeSuite(TicketTestCase,'test')
+
+
+if __name__ == '__main__':
+    unittest.main()
