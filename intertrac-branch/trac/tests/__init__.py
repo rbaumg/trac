6c66d22e91a2a93799c531579c60474122b5c9dc
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2005-04-27 15:57:48 +0000

    InterTrac branch (#234): re-basing with current trunk
    git-svn-id: https://svn.edgewall.org/repos/trac/branches/cboos-dev@1565 af82e41b-90c4-0310-8c96-b1721e28e2e2

---
+++ intertrac-branch/trac/tests/__init__.py
@@ -0,0 +1,18 @@
+import unittest
+
+from trac.tests import config, db, env, perm, query, ticket, wiki
+
+def suite():
+
+    suite = unittest.TestSuite()
+    suite.addTest(config.suite())
+    suite.addTest(db.suite())
+    suite.addTest(env.suite())
+    suite.addTest(perm.suite())
+    suite.addTest(query.suite())
+    suite.addTest(ticket.suite())
+    suite.addTest(wiki.suite())
+    return suite
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
