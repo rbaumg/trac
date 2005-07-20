773f52369682bb53d101e046036fac75a28d5eca
cmlenz <cmlenz@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2005-07-20 00:18:25 +0000

    Trac web-admin plugin: tell {{{setup.py}}} about the additional static resources.
    git-svn-id: https://svn.edgewall.org/repos/trac/branches/jonas-dev@2009 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- webadmin/setup.py
+++ webadmin/setup.py
@@ -6,5 +6,5 @@ PACKAGE = 'TracWebAdmin'
 VERSION = '0.1'
 
 setup(name=PACKAGE, version=VERSION, packages=['webadmin'],
-      package_data={'webadmin': ['templates/*.cs', 'htdocs/css/*.css']})
-
+      package_data={'webadmin': ['htdocs/css/*.css', 'htdocs/img/*.png',
+                                 'htdocs/js/*.js', 'templates/*.cs']})
