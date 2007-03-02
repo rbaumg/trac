rbaumg/trac

commit 6542d6c3f984f01d4ce5ea7d8ebff5f2e3770bf8

Make it possible to examine ticket description changes (#2945).
It is now possible to look at the history of the ticket description field and to visualize the actual changes.
It is also possible to view a ticket at a specified version, in read-only mode.

Next to the ''Description (last modified by <author>)'' title, there's now a (diff) link for showing the differences.
Each description change in the regular change history also features such a (diff) link.

From the diff page, one can go to a ''Ticket History'' view similar to the one of the Wiki page history, this one listing the changes for the ticket description field. From the history view, on can jump to a specific version of the ticket.


git-svn-id: https://svn.edgewall.org/repos/trac/trunk@4347 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/ticket/web_ui.py
@@ -29,9 +29,11 @@ from trac.ticket import Milestone, Ticket, TicketSystem, ITicketManipulator
 from trac.ticket.notification import TicketNotifyEmail
 from trac.timeline.api import ITimelineEventProvider, TimelineEvent
 from trac.util import get_reporter_id
+from trac.util.compat import any
 from trac.util.datefmt import to_timestamp, utc
 from trac.util.html import html, Markup
 from trac.util.text import CRLF, shorten_line
+from trac.versioncontrol.diff import get_diff_options, diff_blocks
 from trac.web import IRequestHandler
 from trac.web.chrome import add_link, add_stylesheet, INavigationContributor, \
                             Chrome
@@ -170,7 +172,8 @@ class TicketModule(Component):
         req.perm.require('TICKET_VIEW')
         data = {}
 
-        action = req.args.get('action', 'view')
+        action = req.args.get('action', ('history' in req.args and 'history' or
+                                         'view'))
 
         db = self.env.get_db_cnx()
         id = int(req.args.get('id'))
@@ -178,7 +181,19 @@ class TicketModule(Component):
         ticket = Ticket(self.env, id, db=db)
         data['ticket'] = ticket
 
-        if req.method == 'POST':
+        if action in ('history', 'diff'):
+            field = req.args.get('field')
+            if field:
+                text_fields = [field]
+            else:
+                text_fields = [field['name'] for field in 
+                               TicketSystem(self.env).get_ticket_fields() if
+                               field['type'] == 'textarea']
+            if action == 'history':
+                return self._render_history(req, db, ticket, data, text_fields)
+            elif action == 'diff':
+                return self._render_diff(req, db, ticket, data, text_fields)
+        elif req.method == 'POST':
             if 'preview' not in req.args:
                 self._do_save(req, db, ticket)
             else:
@@ -235,6 +250,173 @@ class TicketModule(Component):
                      conversion[3])
 
         return 'ticket_view.html', data, None
+
+    def _get_history(self, ticket, db):
+        history = []
+        for change in self.grouped_changelog_entries(ticket, db):
+            if change['permanent']:
+                change['version'] = change['cnum']
+                history.append(change)
+        return history
+        
+    def _render_history(self, req, db, ticket, data, text_fields):
+        """Extract the history for a ticket description.
+
+        """
+        req.perm.require('TICKET_VIEW')
+
+        history = self._get_history(ticket, db)
+        history.reverse()
+        history = [c for c in history if any([f in text_fields
+                                              for f in c['fields']])]
+        history.append({'version': 0, 'comment': "''Initial version''",
+                        'date': ticket.time_created,
+                        'author': ticket['reporter'] # not 100% accurate...
+                        })
+        data.update({'title': 'Ticket History', 'history': history})
+
+        add_link(req, 'up', req.href.ticket(ticket.id))
+        
+        return 'ticket_history.html', data, None
+
+    def _render_diff(self, req, db, ticket, data, text_fields):
+        """Show differences between two versions of a ticket description.
+
+        `text_fields` is optionally a list of fields of interest, that are
+        considered for jumping to the next change.
+        """
+        req.perm.require('TICKET_VIEW')
+
+        new_version = int(req.args.get('version', 1))
+        old_version = int(req.args.get('old_version', new_version))
+        if old_version > new_version:
+            old_version, new_version = new_version, old_version
+
+        # get the list of versions having a description change
+        history = self._get_history(ticket, db)
+        changes = {}
+        descriptions = []
+        old_idx = new_idx = -1 # indexes in descriptions
+        for change in history:
+            version = change['version']
+            changes[version] = change
+            if any([f in text_fields for f in change['fields']]):
+                if old_version and version <= old_version:
+                    old_idx = len(descriptions)
+                if new_idx == -1 and new_version and version >= new_version:
+                    new_idx = len(descriptions)
+                descriptions.append((version, change))
+
+        # determine precisely old and new versions
+        if old_version == new_version:
+            if new_idx >= 0:
+                old_idx = new_idx - 1
+        if old_idx >= 0:
+            old_version, old_change = descriptions[old_idx]
+        else:
+            old_version, old_change = 0, None
+        num_changes = new_idx - old_idx
+        if new_idx >= 0:
+            new_version, new_change = descriptions[new_idx]
+        else:
+            raise TracError('No differences to show')
+
+        # determine prev and next versions
+        prev_version = old_version
+        next_version = None
+        if new_idx < len(descriptions) - 1:
+            next_version = descriptions[new_idx+1][0]
+
+        # -- old properties (old_ticket) and new properties (new_ticket)
+
+        # assume a linear sequence of change numbers, starting at 1, with gaps
+        def replay_changes(values, old_values, from_version, to_version):
+            for version in range(from_version, to_version+1):
+                if version in changes:
+                    for k, v in changes[version]['fields'].iteritems():
+                        values[k] = v['new']
+                        if old_values is not None and k not in old_values:
+                            old_values[k] = v['old']
+
+        old_ticket = {}
+        if old_version:
+            replay_changes(old_ticket, None, 1, old_version)
+
+        new_ticket = {}
+        replay_changes(new_ticket, old_ticket, old_version+1, new_version)
+
+        changes = []
+
+        def version_info(v, field=None):
+            path = 'Ticket #%d' % ticket.id
+            if field:
+                path += Markup(' &ndash; %s', field)
+            if v:
+                rev, shortrev = 'Version %d' % v, 'v%d' % v
+            else:
+                rev, shortrev = 'Initial Version', 'initial'
+            return {'path':  path, 'rev': rev, 'shortrev': shortrev,
+                    'href': req.href.ticket(ticket.id, version=v)}
+
+        # -- prop changes
+        props = []
+        for k, v in new_ticket.iteritems():
+            if k not in text_fields:
+                old, new = old_ticket[k], new_ticket[k]
+                if old != new:
+                    props.append({'name': k, 'old': old, 'new': new})
+        changes.append({'props': props,
+                        'new': version_info(new_version),
+                        'old': version_info(old_version)})
+
+
+        # -- text diffs
+        diff_style, diff_options, diff_data = get_diff_options(req)
+        context = 3
+        for option in diff_options:
+            if option.startswith('-U'):
+                context = int(option[2:])
+                break
+        if context < 0:
+            context = None
+
+        for field in text_fields:
+            old_text = old_ticket.get(field)
+            old_text = old_text and old_text.splitlines() or []
+            new_text = new_ticket.get(field)
+            new_text = new_text and new_text.splitlines() or []
+            diffs = diff_blocks(old_text, new_text, context=context,
+                                ignore_blank_lines='-B' in diff_options,
+                                ignore_case='-i' in diff_options,
+                                ignore_space_changes='-b' in diff_options)
+
+            changes.append({'diffs': diffs,
+                            'new': version_info(new_version, field),
+                            'old': version_info(old_version, field)})
+
+        # -- prev/up/next links
+        if prev_version:
+            add_link(req, 'prev', req.href.ticket(ticket.id, action='diff',
+                                                  version=prev_version),
+                     'Version %d' % prev_version)
+        add_link(req, 'up', req.href.ticket(ticket.id, action='history'),
+                 'Ticket History')
+        if next_version:
+            add_link(req, 'next', req.href.ticket(ticket.id, action='diff',
+                                                  version=next_version),
+                     'Version %d' % next_version)
+
+        add_stylesheet(req, 'common/css/diff.css')
+
+        data.update({
+            'title': 'Ticket Diff',
+            'old_version': old_version, 'new_version': new_version,
+            'changes': changes, 'diff': diff_data,
+            'num_changes': num_changes, 'change': new_change,
+            'old_ticket': old_ticket, 'new_ticket': new_ticket
+            })
+        
+        return 'ticket_diff.html', data, None
 
     # ISearchSource methods
 
@@ -531,14 +713,24 @@ class TicketModule(Component):
     def _insert_ticket_data(self, req, db, ticket, data, reporter_id):
         """Insert ticket data into the hdf"""
         replyto = req.args.get('replyto')
+        version = req.args.get('version', None)
+        
         data['replyto'] = replyto
+        if version:
+            try:
+                version = int(version)
+                data['version'] = version
+            except ValueError:
+                pass
 
         # -- Ticket fields
-
-        data['fields'] = []
+        types = {}
+        fields = []
         for field in TicketSystem(self.env).get_ticket_fields():
             name = field['name']
-            if field['type'] in ('radio', 'select'):
+            type_ = field['type']
+            types[name] = type_
+            if type_ in ('radio', 'select'):
                 value = ticket.values.get(field['name'])
                 options = field['options']
                 if name == 'milestone' and 'TICKET_ADMIN' not in req.perm:
@@ -552,7 +744,7 @@ class TicketModule(Component):
             if name in ('summary', 'reporter', 'description', 'status',
                         'resolution', 'owner'):
                 field['skip'] = True
-            data['fields'].append(field)
+            fields.append(field)
 
         data['reporter_id'] = reporter_id
 
@@ -572,45 +764,48 @@ class TicketModule(Component):
         if replyto == 'description':
             quote_original(ticket['reporter'], ticket['description'],
                            'ticket:%d' % ticket.id)
+        values = {}
         replies = {}
         changes = []
         cnum = 0
-        description_lastmod = description_author = None
         for change in self.grouped_changelog_entries(ticket, db):
+            if version is not None and cnum > version:
+                # Retrieve initial ticket values from later changes
+                for k, v in change['fields'].iteritems():
+                    if k not in values:
+                        values[k] = v['old']
+                continue
             changes.append(change)
-            # wikify comment
-            comment = ''
             if change['permanent']:
                 cnum = change['cnum']
                 # keep track of replies threading
                 if 'replyto' in change:
                     replies.setdefault(change['replyto'], []).append(cnum)
                 # eventually cite the replied to comment
+                comment = ''
                 if replyto == str(cnum):
                     quote_original(change['author'], comment,
                                    'comment:%s' % replyto)
-            if 'description' in change['fields']:
-                change['fields']['description'] = ''
-                description_lastmod = change['date']
-                description_author = change['author']
+                if version:
+                    # Override ticket value by current changes
+                    for k, v in change['fields'].iteritems():
+                        values[k] = v['new']
+                if 'description' in change['fields']:
+                    data['description_change'] = change
 
-        data['changes'] = changes
-        data['replies'] = replies
-        data['cnum'] = cnum + 1
-        if description_lastmod:
-            data['description_author'] = description_author
-            data['description_lastmod'] = description_lastmod
+        if version is not None:
+            ticket.values.update(values)
 
-        # -- Ticket Attachments
-
-        data['attachments'] = list(Attachment.select(self.env, 'ticket',
-                                                     ticket.id))
-        if 'TICKET_APPEND' in req.perm:
-            data['attach_href'] = req.href.attachment('ticket', ticket.id)
-
-        # Add the possible actions to hdf
-        actions = TicketSystem(self.env).get_available_actions(ticket, req.perm)
-        data['actions'] = actions
+        data.update({
+            'fields': fields, 'changes': changes, 'field_types': types,
+            'replies': replies, 'cnum': cnum + 1,
+            'attachments': list(Attachment.select(self.env, 'ticket',
+                                                  ticket.id)),
+            'attach_href': 'TICKET_APPEND' in req.perm and \
+            req.href.attachment('ticket', ticket.id),
+            'actions': TicketSystem(self.env).get_available_actions(ticket,
+                                                                    req.perm)
+            })
 
     def grouped_changelog_entries(self, ticket, db, when=None):
         """Iterate on changelog entries, consolidating related changes

trac/wiki/web_ui.py
@@ -471,4 +471,5 @@ class WikiModule(Component):
 
         for name, ts, author, text in cursor:
             yield (req.href.wiki(name), '%s: %s' % (name, shorten_line(text)),
-                   datetime.fromtimestamp(ts, utc), author, shorten_result(text, terms))
+                   datetime.fromtimestamp(ts, utc), author,
+                   shorten_result(text, terms))

htdocs/css/trac.css
@@ -422,6 +422,16 @@ table.listing tbody tr.even { background-color: #fcfcfc }
 table.listing tbody tr.odd { background-color: #f7f7f7 }
 table.listing tbody tr:hover { background: #eed !important }
 
+/* Styles for the page history table
+   (extends the styles for "table.listing") */
+#fieldhist td { padding: 0 .5em }
+#fieldhist td.date, #fieldhist td.diff, #fieldhist td.version,
+#fieldhist td.author {
+ white-space: nowrap;
+}
+#fieldhist td.version { text-align: center }
+#fieldhist td.comment { width: 100% }
+
 /* Auto-completion interface */
 .suggestions { background: #fff; border: 1px solid #886; color: #222; }
 .suggestions ul {

htdocs/css/wiki.css
@@ -23,16 +23,6 @@
 #overview .ipnr { color: #999; font-size: 80% }
 #overview .comment { padding: 1em 0 0 }
 
-/* Styles for the page history table
-   (extends the styles for "table.listing") */
-#wikihist td { padding: 0 .5em }
-#wikihist td.date, #wikihist td.diff, #wikihist td.version,
-#wikihist td.author {
- white-space: nowrap;
-}
-#wikihist td.version { text-align: center }
-#wikihist td.comment { width: 100% }
-
 @media print { 
  th.diff, td.diff { display: none }
 }

templates/ticket_diff.html
@@ -1,0 +1,77 @@
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"
+      xmlns:py="http://genshi.edgewall.org/"
+      xmlns:xi="http://www.w3.org/2001/XInclude">
+  <xi:include href="layout.html" />
+  <xi:include href="macros.html" />
+  <head>
+    <title>$title</title>
+    <link rel="stylesheet" type="text/css" href="${chrome.htdocs_location}css/diff.css" />
+    <meta name="ROBOTS" content="NOINDEX, NOFOLLOW" />
+  </head>
+
+  <body>
+    <div id="ctxtnav" class="nav">
+      <h2>Ticket Navigation</h2>
+      ${prevnext_nav('Change', 'Ticket History')}
+      <hr />
+    </div>
+
+    <div id="content" class="ticket">
+      <h1 py:choose="">Changes
+        <py:when test="old_version">between
+          <a href="${href.ticket(ticket.id,version=old_version)}">Version $old_version</a> and
+        </py:when>
+        <py:when test="old_version == 0">between
+          <a href="${href.ticket(ticket.id,version=0)}">Initial Version</a> and
+        </py:when>
+        <py:otherwise>from</py:otherwise>
+        <a href="${href.ticket(ticket.id,version=new_version)}">Version $new_version</a> of
+        <a href="${href.ticket(ticket.id)}">Ticket #$ticket.id</a>
+      </h1>
+      <form method="post" id="prefs"
+            action="${href.ticket(ticket.id,action='diff',old_version=old_version,version=new_version)}">
+        <div>
+          <input type="hidden" name="action" value="diff" />
+          <input type="hidden" name="version" value="$new_version" />
+          <input type="hidden" name="old_version" value="$old_version" />
+          ${diff_options_fields(diff)}
+        </div>
+      </form>
+      <dl id="overview" py:with="multi = num_changes &gt; 1">
+        <dt class="property time">Timestamp:</dt>
+        <dd class="time" py:choose="">
+          <em py:when="multi" class="multi">(multiple changes)</em>
+          <py:when test="date">${format_datetime(change.date)} (${pretty_timedelta(change.date)} ago)</py:when>
+          <py:otherwise>--</py:otherwise>
+        </dd>
+        <dt class="property author">Author:</dt>
+        <dd class="author" py:choose="">
+          <em py:when="multi" class="multi">(multiple changes)</em>
+          <py:otherwise>$change.author <span py:if="change.ipnr" class="ipnr">(IP: $change.ipnr)</span></py:otherwise>
+        </dd>
+        <dt class="property message">Comment:</dt>
+        <dd class="message" py:choose="">
+          <em py:when="multi" class="multi">(multiple changes)</em>
+          <py:otherwise>${wiki_to_html(change.comment)}</py:otherwise>
+        </dd>
+      </dl>
+      <div class="diff">
+        <div id="legend">
+          <h3>Legend:</h3>
+          <dl>
+            <dt class="unmod"></dt><dd>Unmodified</dd>
+            <dt class="add"></dt><dd>Added</dd>
+            <dt class="rem"></dt><dd>Removed</dd>
+            <dt class="mod"></dt><dd>Modified</dd>
+          </dl>
+        </div>
+
+        <xi:include href="diff_div.html"/>
+
+      </div>
+    </div>
+  </body>
+</html>

templates/ticket_history.html
@@ -11,15 +11,18 @@
   </head>
 
   <body>
-    <div id="ctxtnav" class="nav">
-      <h2>Wiki Navigation</h2>
+    <div id="ctxtnav" class="nav" py:with="links = chrome.links">
+      <h2>Ticket Navigation</h2>
       <ul>
-        <li class="last"><a href="${href.wiki(page.name)}">View Latest Version</a></li>
+        <li class="last"><a py:with="link = links.up[0]" href="${link.href}"
+            title="${link.title}">Back to Ticket #${ticket.id}</a>
+        </li>
       </ul>
     </div>
 
-    <div id="content" class="wiki">
-      <h1>Change History of <a href="${href.wiki(page.name)}">$page_name</a></h1>
+    <div id="content" class="ticket">
+      <h1>Change History for Ticket #${ticket.id}</h1>
+
       <form py:if="history" class="printableform" method="get" action="">
         <input type="hidden" name="action" value="diff" />
         <div class="buttons">
@@ -44,7 +47,7 @@
                        checked="${idx == 0 or None}" />
               </td>
               <td class="version">
-                <a href="${href.wiki(page.name, version=item.version)}" title="View this version">$item.version</a>
+                <a href="${href.ticket(ticket.id, version=item.version)}" title="View this version">$item.version</a>
               </td>
               <td class="date">${format_datetime(item.date)}</td>
               <td class="author" title="IP-Address: $item.ipnr">$item.author</td>

templates/ticket_view.html
@@ -28,10 +28,19 @@
     </div>
 
     <div id="content" class="ticket">
-      <h1>Ticket #${ticket.id}
+      <h1><a py:strip="not version and version != 0" href="${href.ticket(ticket.id)}">Ticket #${ticket.id}</a>
         <span class="status">(${ticket.status}<py:if
             test="ticket.type"> ${ticket.type}</py:if><py:if
             test="ticket.resolution">: ${ticket.resolution}</py:if>)</span>
+        <py:choose test="version">
+          <py:when test="0">
+            &mdash; at <a href="#comment:description">Initial Version</a>
+          </py:when>
+          <py:when test="Undefined" />
+          <py:otherwise>
+            &mdash; at <a href="#comment:$version">Version $version</a>
+          </py:otherwise>
+        </py:choose>
       </h1>
 
       <div id="ticket">
@@ -70,12 +79,13 @@
             <h3 id="comment:description">
               <span py:if="'TICKET_APPEND' in perm" class="inlinebuttons">
                 <input type="hidden" name="replyto" value="description" />
-                <input type="submit" value="Reply" title="Reply, quoting this description" />
+                <input type="submit" name="reply" value="Reply" title="Reply, quoting this description" />
               </span>
               Description
-              <span py:if="description_lastmod" class="lastmod"
-                title="description_lastmod">
-                (last modified by ${description_author})
+              <span py:if="description_change" class="lastmod"
+                title="$description_change.date">
+                (last modified by ${description_change.author})
+                (<a href="${href.ticket(ticket.id, action='diff', version=description_change.cnum)}">diff</a>)
               </span>
             </h3>
             <div class="searchable">
@@ -85,7 +95,9 @@
         </form>
       </div>
 
-      ${list_of_attachments(attachments, attach_href)}
+      <py:if test="not version and version != 0">
+        ${list_of_attachments(attachments, attach_href)}
+      </py:if>
 
       <py:if test="changes">
         <h2>Change History</h2>
@@ -117,17 +129,29 @@
                 ${change.date} changed by ${change.author}
               </h3>
               <ul py:if="change.fields" class="changes">
-                <li py:for="field_name, field in change.fields.items()">
+                <li py:for="field_name, field in change.fields.items()"
+                    py:with="filed_type = field_types[field_name]">
                   <strong>${field_name}</strong>
                   <py:choose>
                     <py:when test="field_name == 'attachment'">
-                      <em>${field.new}</em> added
+                      <a href="${href.attachment('ticket', ticket.id, field.new)}"><em>${field.new}</em></a> added
                     </py:when>
                     <py:when test="field.old and field.new">
-                      changed from <em>${field.old}</em> to <em>${field.new}</em>
+                      changed
+                      <py:choose>
+                        <py:when test="field_types[field_name] == 'textarea'">
+                          (<a href="${href.ticket(ticket.id, action='diff', version=change.cnum)}">diff</a>)
+                        </py:when>
+                        <py:otherwise>
+                          from <em>${field.old}</em> to <em>${field.new}</em>
+                        </py:otherwise>
+                      </py:choose>
                     </py:when>
                     <py:when test="not field.old and field.new">
-                      set to <em>${field.new}</em>
+                      set
+                      <py:if test="field_types[field_name] != 'textarea'">
+                        to <em>${field.new}</em>
+                      </py:if>
                     </py:when>
                     <py:otherwise>deleted</py:otherwise>
                   </py:choose>
@@ -141,7 +165,7 @@
         </div>
       </py:if>
 
-      <form py:if="'TICKET_APPEND' in perm or 'TICKET_CHGPROP' in perm"
+      <form py:if="not version and version != 0 and ('TICKET_APPEND' in perm or 'TICKET_CHGPROP' in perm)"
             action="#preview" method="post">
         <hr />
         <h3><a name="edit" onfocus="$('#comment').get(0).focus()">

commit 65c65f9bbd90b892961a838e90f4bd256db72c1b

Fix the ''Reply to'' comment, which was apparently broken since r4347.
git-svn-id: https://svn.edgewall.org/repos/trac/trunk@4934 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/ticket/web_ui.py
@@ -836,9 +836,8 @@ class TicketModule(Component):
                     if 'replyto' in change:
                         replies.setdefault(change['replyto'], []).append(cnum)
                     # eventually cite the replied to comment
-                    comment = ''
                     if replyto == str(cnum):
-                        quote_original(change['author'], comment,
+                        quote_original(change['author'], change['comment'],
                                        'comment:%s' % replyto)
                     if version:
                         # Override ticket value by current changes