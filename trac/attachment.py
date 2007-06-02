rbaumg/trac

commit f8f841b6cb72d525af54929431ce809cec082f10

Simplify steps needed for displaying lists of attachments, by introducing an `AttachmentList` helper class
git-svn-id: https://svn.edgewall.org/repos/trac/trunk@5568 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/attachment.py
@@ -90,7 +90,7 @@ class AttachmentContext(Context):
         return Attachment(self.env, self.parent.realm, self.parent.id,
                           filename=self.id, db=self._db)
 
-    def resource_href(self, path=None, **kwargs):
+    def get_href(self, href, path=None, **kwargs):
         """Return an URL to the attachment itself.
 
         A `format` keyword argument equal to `'raw'` will be converted
@@ -103,7 +103,7 @@ class AttachmentContext(Context):
             prefix = 'raw-attachment'
         path = [unicode(p) for p in [prefix, self.parent.realm, self.parent.id,
                                      self.id, path] if p]
-        return Context.resource_href(self, '/' + '/'.join(path), **kwargs)
+        return Context.get_href(self, href, '/' + '/'.join(path), **kwargs)
 
     def permid(self):
         return self.parent.permid() + (self.realm, self.id)
@@ -120,6 +120,22 @@ class AttachmentContext(Context):
     def summary(self):
         return self.resource.description
 
+
+class AttachmentList(object):
+    """Helper class for template data, representing a list of attachments."""
+    
+    def __init__(self, parent, attachments):
+        self.parent = parent
+        self.attachments = [parent('attachment', a.filename, resource=a)
+                            for a in attachments]
+
+    def attach_href(self):
+        return self.parent('attachment').get_href(self.parent.req.href,
+                                                  action='new')
+
+    def has_perm(self, action, attachment_context=None):
+        return AttachmentModule(self.parent.env).has_perm(
+            action, self.parent.req, attachment_context or self.parent)
 
 class Attachment(object):
 
@@ -378,8 +394,7 @@ class AttachmentModule(Component):
             parent_id = '/'.join(segments[:-1])
             filename = len(segments) > 1 and segments[-1]
             if not filename: # if there's a trailing '/', show the list
-                return self._render_list(context(parent_realm, parent_id) \
-                                         ('attachment'))
+                return self._render_list(context(parent_realm, parent_id))
             attachment = Attachment(self.env, parent_realm, parent_id,
                                     filename)
 
@@ -414,6 +429,10 @@ class AttachmentModule(Component):
         yield ('attachment', self._format_link)
 
     # Public methods
+
+    def attachment_list(self, parent):
+        return AttachmentList(
+            parent, Attachment.select(self.env, parent.realm, parent.id))
 
     def require_perm(self, action, req, context):
         if context.realm != 'attachment':
@@ -572,17 +591,14 @@ class AttachmentModule(Component):
         return {'mode': 'new', 'author': get_reporter_id(context.req)}
 
     def _render_list(self, context):
-        req, attachment = context.req, context.resource
-        self.require_perm('VIEW', req, context)
+        self.require_perm('VIEW', context.req, context)
 
         data = {
             'mode': 'list', 'context': context,
-            'attachments': Attachment.select(self.env, context.parent.realm,
-                                             context.parent.id),
-            'attachment_perm': AttachmentModule(self.env).has_perm,            
+            'attachments': self.attachment_list(context)
             }
 
-        add_link(req, 'up', context.parent.resource_href(),
+        add_link(context.req, 'up', context.parent.resource_href(),
                  context.parent.name())
         
         return 'attachment.html', data, None

trac/templates/attachment.html
@@ -76,7 +76,7 @@
 
       <py:when test="'list'">
         <h1><a href="$parent_href">$parent_name</a></h1>
-        ${list_of_attachments(context.parent, attachments, attachment_perm)}
+        ${list_of_attachments(attachments)}
       </py:when>
 
       <py:otherwise> <!--! 'render' mode -->

trac/templates/macros.html
@@ -158,59 +158,55 @@
 
   <!--! Display a list of attachments
   -
-  -     'attachments' is a list of attachment data
-  -     'attachment_perm' is a function for check permissions for actions on attachments
+  -     'alist' is an AttachmentList object (see attachment.py)
   -     'compact' is used to switch between a compact list of attachments (e.g. as used for
   -       the wiki view) and an expanded list (e.g. as used for the ticket view)
   -->
-  <py:def function="list_of_attachments(context, attachments, attachment_perm, compact=False)">
-    <py:def function="show_one_attachment(attachment)">
-      <a href="${context.href('attachment', context.realm, context.id, attachment.filename)}"
-         title="View attachment">$attachment.filename</a>
-      (${sizeinfo(attachment.size)}) - added by <em>${authorinfo(attachment.author)}</em>
-      ${dateinfo(attachment.date)} ago.
+  <py:def function="list_of_attachments(alist, compact=False)">
+    <py:def function="show_one_attachment(ctx)">
+      <py:with vars="attachment = ctx.resource">
+        <a href="${ctx.get_href(href)}" title="View attachment">$attachment.filename</a>
+        (${sizeinfo(attachment.size)}) - added by <em>${authorinfo(attachment.author)}</em>
+        ${dateinfo(attachment.date)} ago.
+      </py:with>
     </py:def>
     <py:choose test="">
-      <py:when test="compact and attachments">
+      <py:when test="compact and alist.attachments">
         <h3>Attachments</h3>
         <ul>
-          <py:for each="attachment in attachments" py:with="ctx = context('attachment', attachment.filename)">
-            <li py:if="attachment_perm('VIEW',req, ctx)">
-              ${show_one_attachment(attachment)}
-              <q py:if="compact and attachment.description">${wiki_to_oneliner(ctx, attachment.description)}</q>
+          <py:for each="ctx in alist.attachments">
+            <li py:if="attachments.has_perm('VIEW', ctx)">
+              ${show_one_attachment(ctx)}
+              <q py:if="compact and ctx.resource.description">${wiki_to_oneliner(ctx, ctx.resource.description)}</q>
             </li>
           </py:for>
         </ul>
       </py:when>
       <py:when test="not compact">
         <h2>Attachments</h2>
-        <div py:if="attachments or attach_href" id="attachments">
-          <dl py:if="attachments" class="attachments">
-            <py:for each="attachment in attachments"
-              py:with="ctx = context('attachment', attachment.filename)">
-              <py:if test="attachment_perm('VIEW',req, ctx)">
-                <dt>${show_one_attachment(attachment)}</dt>
-                <dd py:if="attachment.description">
-                  ${wiki_to_oneliner(ctx, attachment.description)}
+        <div py:if="alist.attachments or alist.has_perm('CREATE')" id="attachments">
+          <dl py:if="alist.attachments" class="attachments">
+            <py:for each="ctx in alist.attachments">
+              <py:if test="alist.has_perm('VIEW', ctx)">
+                <dt>${show_one_attachment(ctx)}</dt>
+                <dd py:if="ctx.resource.description">
+                  ${wiki_to_oneliner(ctx, ctx.resource.description)}
                 </dd>
               </py:if>
             </py:for>
           </dl>
-          <form py:if="attachment_perm('CREATE',req, context)" method="get"
-            action="${context.href('attachment', context.realm, context.id, action='new')}">
-            <div>
-              <input type="hidden" name="action" value="new" />
-              <input type="submit" value="Attach File" />
-            </div>
-          </form>
+          ${attach_file_form(alist)}
         </div>
       </py:when>
     </py:choose>
   </py:def>
 
-  <py:def function="attach_file_form(context, attachment_perm)">
-    <form py:if="attachment_perm('CREATE', req, context)" method="get"
-          action="${context.href('attachment', context.realm, context.id, action='new')}">
+  <!--! Conditionally render an ''Attach File'' button
+  -
+  -     'alist' is an AttachmentList object (see attachment.py)
+  -->
+  <py:def function="attach_file_form(alist)">
+    <form py:if="alist.has_perm('CREATE')" method="get" action="${alist.attach_href()}">
       <div>
         <input type="hidden" name="action" value="new" />
         <input type="submit" value="Attach file" />

trac/ticket/templates/ticket.html
@@ -217,7 +217,7 @@
       <py:if test="ticket.exists">
         <!--! do not show attachments for old versions of this ticket or for new tickets -->
         <py:if test="not version and version != 0 and ticket.exists">
-          ${list_of_attachments(context, attachments, attachment_perm)}
+          ${list_of_attachments(attachments)}
         </py:if>
 
         <py:if test="ticket.exists and changes">
@@ -369,9 +369,9 @@ ${comment}</textarea></p>
           </fieldset>
         </py:if>
 
-        <p py:if="not ticket.exists and can_attach">
+        <p py:if="not ticket.exists and attachments.has_perm('CREATE')">
           <label>
-            <input type="checkbox" name="attachment" checked="${attachment or None}" />
+            <input type="checkbox" name="attachment" checked="${'attachment' in req.args or None}" />
             I have files to attach to this ticket
           </label>
         </p>

trac/ticket/web_ui.py
@@ -24,7 +24,7 @@ import time
 from genshi.core import Markup
 from genshi.builder import tag
 
-from trac.attachment import Attachment, AttachmentModule
+from trac.attachment import AttachmentModule
 from trac.config import BoolOption, Option, IntOption
 from trac.context import Context
 from trac.core import *
@@ -358,12 +358,6 @@ class TicketModule(Component):
             field.setdefault('skip', False)
             ticket.values.setdefault(name, field.get('value'))
             data['fields'].append(field)
-
-        if 'TICKET_APPEND' in req.perm:
-            data['can_attach'] = True
-            data['attachment'] = req.args.get('attachment')
-        else:
-            data['can_attach'] = False
 
         add_stylesheet(req, 'common/css/ticket.css')
         return 'ticket.html', data, None
@@ -1038,9 +1032,7 @@ class TicketModule(Component):
         data.update({
             'fields': fields, 'changes': changes, 'field_types': types,
             'replies': replies, 'cnum': cnum + 1,
-            'attachments': list(Attachment.select(self.env, 'ticket',
-                                                  ticket.id)),
-            'attachment_perm': AttachmentModule(self.env).has_perm,
+            'attachments': AttachmentModule(self.env).attachment_list(context),
             'action_controls': action_controls,
             'action': selected_action,
             'change_preview': change_preview

trac/wiki/templates/wiki_view.html
@@ -59,7 +59,7 @@
         </py:otherwise>
       </div>
 
-      ${list_of_attachments(context, attachments, attachment_perm, compact=True)}
+      ${list_of_attachments(attachments, compact=True)}
 
       <py:with vars="modify_perm = 'WIKI_MODIFY' in perm(context);
                      delete_perm = 'WIKI_DELETE' in perm(context);
@@ -90,7 +90,7 @@
                 </div>
               </form>
               <py:if test="page.exists">
-                ${attach_file_form(context, attachment_perm)}
+                ${attach_file_form(attachments)}
               </py:if>
             </py:if>
             <py:if test="page.exists and delete_perm">

trac/wiki/web_ui.py
@@ -24,7 +24,7 @@ import StringIO
 
 from genshi.builder import tag
 
-from trac.attachment import Attachment, AttachmentModule
+from trac.attachment import AttachmentModule
 from trac.context import Context, ResourceSystem, ResourceNotFound
 from trac.core import *
 from trac.mimeview.api import Mimeview, IContentConverter
@@ -514,8 +514,7 @@ class WikiModule(Component):
 
         data.update({
             'latest_version': latest_page.version,
-            'attachments': Attachment.select(self.env, 'wiki', page.name),
-            'attachment_perm': AttachmentModule(self.env).has_perm,
+            'attachments': AttachmentModule(self.env).attachment_list(context),
             'default_template': self.DEFAULT_PAGE_TEMPLATE,
             'templates': templates,
             'version': version

commit 121ad26601993c3076de249d12a77bfc7a4e5712

Fix a minor glitch introduced by r5568 for displaying the parent name in the attachment template in list mode
git-svn-id: https://svn.edgewall.org/repos/trac/trunk@5569 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/attachment.py
@@ -594,12 +594,11 @@ class AttachmentModule(Component):
         self.require_perm('VIEW', context.req, context)
 
         data = {
-            'mode': 'list', 'context': context,
+            'mode': 'list', 'context': None, # no specific attachment
             'attachments': self.attachment_list(context)
             }
 
-        add_link(context.req, 'up', context.parent.resource_href(),
-                 context.parent.name())
+        add_link(context.req, 'up', context.resource_href(), context.name())
         
         return 'attachment.html', data, None
 

trac/templates/attachment.html
@@ -10,8 +10,9 @@
     <title>Attachment</title>
   </head>
 
-  <body py:with="parent_name = context.parent.name();
-                 parent_href = context.parent.resource_href()">
+  <body py:with="parent = attachments and attachments.parent or context.parent;
+                 parent_name = parent.name();
+                 parent_href = parent.get_href(href)">
 
     <div id="ctxtnav" class="nav">
       <h2>Attachment Navigation</h2>